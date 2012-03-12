e637f30dc15bf1020b27177644ba36f683ff1c55
ccnusjy@gmail.com <ccnusjy@gmail.com@cebc5354-bb2e-e4fc-d3b7-20e417a0e997>
2012-03-12 13:02:48 +0000

    first commit

---
+++ trunk/sdict/build.sh
@@ -0,0 +1,1 @@
+/opt/python2.5/bin/python setup.py build_ext --inplace
---
+++ trunk/sdict/test.py
@@ -0,0 +1,34 @@
+import tst
+d = tst.create_tst_db()
+print tst.tst_get(d,"xx")
+tst.tst_put(d,"abc",range(100))
+x = tst.tst_get(d,"abc")
+print x
+tst.tst_delete(d,"abc")
+
+for i in xrange(1000):
+	tst.tst_put(d,str(i),"xxxx"+str(i))
+
+result=[]
+key = "16"
+limit = 5 
+
+tst.tst_prefix(d,key,result,limit,tst.DESC)
+print result
+
+result=[]
+limit =5
+tst.tst_less(d,key,result,limit)
+print result
+
+
+
+
+
+
+
+
+
+
+
+
